6b47880bae492e47f8f90802150b5289664d22a6
AOA <aoa@d.co>
2025-12-27 18:38:04 +0200

    #Update 2.0.0

--- src/main/java/net/alminoris/aestheticsurfaces/AestheticSurfaces.java
+++ src/main/java/net/alminoris/aestheticsurfaces/AestheticSurfaces.java
@@ -4,31 +4,19 @@ import com.mojang.logging.LogUtils;
 import net.alminoris.aestheticsurfaces.block.ModBlocks;
 import net.alminoris.aestheticsurfaces.item.ModItemGroups;
 import net.alminoris.aestheticsurfaces.item.ModItems;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
+import net.alminoris.aestheticsurfaces.util.helper.BlockSetsHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -46,7 +34,6 @@ public class AestheticSurfaces
 
         ModBlocks.register(modEventBus);
         ModItems.register(modEventBus);
-        ModItemGroups.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
 
@@ -60,10 +47,134 @@ public class AestheticSurfaces
 
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    private void addCreative(CreativeModeTabEvent.BuildContents entries)
     {
+        if (entries.getTab() == ModItemGroups.ASURF_TAB)
+        {
+            for(String name : BlockSetsHelper.COLORS)
+                entries.accept(ModBlocks.COFFERED_CEILINGS.get(name).get());
 
+            for(String name : BlockSetsHelper.CEILING_TYPES)
+                entries.accept(ModBlocks.SLIM_CEILINGS.get(name).get());
+
+            for(String name : BlockSetsHelper.BRICKS_NAMES)
+                entries.accept(ModBlocks.BRICKS_VENEERS.get(name).get());
+
+            entries.accept(ModBlocks.SMOOTH_STONE_ROAD.get());
+
+            for(String name : BlockSetsHelper.WOODS)
+                for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                    entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
+
+            if (ModList.get().isLoaded("arborealnature"))
+            {
+                for(String name : BlockSetsHelper.EXTRA_WOODS_AN)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
+            }
+
+            if (ModList.get().isLoaded("wildfields"))
+            {
+                for(String name : BlockSetsHelper.EXTRA_WOODS_WF)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
+            }
+
+            if (ModList.get().isLoaded("whisperleaftrees"))
+            {
+                for(String name : BlockSetsHelper.WT_WOOD_NAMES)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
+            }
+
+            if (ModList.get().isLoaded("silverwoodtrees"))
+            {
+                for(String name : BlockSetsHelper.ST_WOOD_NAMES)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
+            }
+
+            if (ModList.get().isLoaded("missingtrees"))
+            {
+                for(String name : BlockSetsHelper.MT_WOOD_NAMES)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
+            }
+
+            if (ModList.get().isLoaded("natures_spirit"))
+            {
+                for(String name : BlockSetsHelper.NSS_WOOD_NAMES)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
+            }
+
+            for(String name : BlockSetsHelper.COLORS)
+                entries.accept(ModBlocks.WALLPAPERS.get(name).get());
+            for(String name : BlockSetsHelper.COLORS)
+                entries.accept(ModBlocks.SIMPLE_CARPETS.get(name).get());
+            for(String name : BlockSetsHelper.COLORS)
+                entries.accept(ModBlocks.SIMPLE_WALLPAPERS.get(name).get());
+            for(String name : BlockSetsHelper.COLORS)
+                entries.accept(ModBlocks.SIMPLE_CARPET_BLOCKS.get(name).get());
+            for(String name : BlockSetsHelper.COLORS)
+                entries.accept(ModBlocks.SMOOTH_CARPETS.get(name).get());
+            for(String name : BlockSetsHelper.COLORS)
+                entries.accept(ModBlocks.SMOOTH_WALLPAPERS.get(name).get());
+            for(String name : BlockSetsHelper.COLORS)
+                entries.accept(ModBlocks.SMOOTH_CARPET_BLOCKS.get(name).get());
+            for(String name : BlockSetsHelper.COLORS)
+                entries.accept(ModBlocks.TRANSITIONAL_CARPETS.get(name).get());
+            for(String name : BlockSetsHelper.COLORS)
+                entries.accept(ModBlocks.TRANSITIONAL_WALLPAPERS.get(name).get());
+            for(String name : BlockSetsHelper.COLORS)
+                entries.accept(ModBlocks.TRANSITIONAL_CARPET_BLOCKS.get(name).get());
+
+            for(String name : BlockSetsHelper.WOODS)
+                for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                    entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
+
+            if (ModList.get().isLoaded("arborealnature"))
+            {
+                for(String name : BlockSetsHelper.EXTRA_WOODS_AN)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
+            }
+
+            if (ModList.get().isLoaded("wildfields"))
+            {
+                for(String name : BlockSetsHelper.EXTRA_WOODS_WF)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
+            }
+
+            if (ModList.get().isLoaded("whisperleaftrees"))
+            {
+                for(String name : BlockSetsHelper.WT_WOOD_NAMES)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
+            }
+
+            if (ModList.get().isLoaded("silverwoodtrees"))
+            {
+                for(String name : BlockSetsHelper.ST_WOOD_NAMES)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
+            }
+
+            if (ModList.get().isLoaded("missingtrees"))
+            {
+                for(String name : BlockSetsHelper.MT_WOOD_NAMES)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
+            }
+
+            if (ModList.get().isLoaded("natures_spirit"))
+            {
+                for(String name : BlockSetsHelper.NSS_WOOD_NAMES)
+                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
+                        entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
+            }
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/alminoris/aestheticsurfaces/item/ModItemGroups.java
+++ src/main/java/net/alminoris/aestheticsurfaces/item/ModItemGroups.java
@@ -5,9 +5,13 @@ import net.alminoris.aestheticsurfaces.block.ModBlocks;
 import net.alminoris.aestheticsurfaces.util.helper.BlockSetsHelper;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -19,141 +23,13 @@ import static net.alminoris.aestheticsurfaces.util.helper.BlockSetsHelper.NSS_WO
 @Mod.EventBusSubscriber(modid = AestheticSurfaces.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModItemGroups
 {
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, AestheticSurfaces.MOD_ID);
+    public static CreativeModeTab ASURF_TAB;
 
-    public static final RegistryObject<CreativeModeTab> ASURF_TAB = CREATIVE_MODE_TABS.register("asurftab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(ModBlocks.SIMPLE_CARPETS.get("white").get().asItem()::getDefaultInstance)
-            .title(Component.translatable("itemgroup.asurftab"))
-            .displayItems((parameters, entries) ->
-            {
-                for(String name : BlockSetsHelper.COLORS)
-                    entries.accept(ModBlocks.COFFERED_CEILINGS.get(name).get());
-
-                for(String name : BlockSetsHelper.CEILING_TYPES)
-                    entries.accept(ModBlocks.SLIM_CEILINGS.get(name).get());
-
-                for(String name : BlockSetsHelper.BRICKS_NAMES)
-                    entries.accept(ModBlocks.BRICKS_VENEERS.get(name).get());
-
-                entries.accept(ModBlocks.SMOOTH_STONE_ROAD.get());
-
-                for(String name : BlockSetsHelper.WOODS)
-                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                        entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
-
-                if (ModList.get().isLoaded("arborealnature"))
-                {
-                    for(String name : BlockSetsHelper.EXTRA_WOODS_AN)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
-                }
-
-                if (ModList.get().isLoaded("wildfields"))
-                {
-                    for(String name : BlockSetsHelper.EXTRA_WOODS_WF)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
-                }
-
-                if (ModList.get().isLoaded("whisperleaftrees"))
-                {
-                    for(String name : BlockSetsHelper.WT_WOOD_NAMES)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
-                }
-
-                if (ModList.get().isLoaded("silverwoodtrees"))
-                {
-                    for(String name : BlockSetsHelper.ST_WOOD_NAMES)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
-                }
-
-                if (ModList.get().isLoaded("missingtrees"))
-                {
-                    for(String name : BlockSetsHelper.MT_WOOD_NAMES)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
-                }
-
-                if (ModList.get().isLoaded("natures_spirit"))
-                {
-                    for(String name : BlockSetsHelper.NSS_WOOD_NAMES)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_CARPETS.get(name+"_"+typeName).get());
-                }
-
-                for(String name : BlockSetsHelper.COLORS)
-                    entries.accept(ModBlocks.WALLPAPERS.get(name).get());
-                for(String name : BlockSetsHelper.COLORS)
-                    entries.accept(ModBlocks.SIMPLE_CARPETS.get(name).get());
-                for(String name : BlockSetsHelper.COLORS)
-                    entries.accept(ModBlocks.SIMPLE_WALLPAPERS.get(name).get());
-                for(String name : BlockSetsHelper.COLORS)
-                    entries.accept(ModBlocks.SIMPLE_CARPET_BLOCKS.get(name).get());
-                for(String name : BlockSetsHelper.COLORS)
-                    entries.accept(ModBlocks.SMOOTH_CARPETS.get(name).get());
-                for(String name : BlockSetsHelper.COLORS)
-                    entries.accept(ModBlocks.SMOOTH_WALLPAPERS.get(name).get());
-                for(String name : BlockSetsHelper.COLORS)
-                    entries.accept(ModBlocks.SMOOTH_CARPET_BLOCKS.get(name).get());
-                for(String name : BlockSetsHelper.COLORS)
-                    entries.accept(ModBlocks.TRANSITIONAL_CARPETS.get(name).get());
-                for(String name : BlockSetsHelper.COLORS)
-                    entries.accept(ModBlocks.TRANSITIONAL_WALLPAPERS.get(name).get());
-                for(String name : BlockSetsHelper.COLORS)
-                    entries.accept(ModBlocks.TRANSITIONAL_CARPET_BLOCKS.get(name).get());
-
-                for(String name : BlockSetsHelper.WOODS)
-                    for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                        entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
-
-                if (ModList.get().isLoaded("arborealnature"))
-                {
-                    for(String name : BlockSetsHelper.EXTRA_WOODS_AN)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
-                }
-
-                if (ModList.get().isLoaded("wildfields"))
-                {
-                    for(String name : BlockSetsHelper.EXTRA_WOODS_WF)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
-                }
-
-                if (ModList.get().isLoaded("whisperleaftrees"))
-                {
-                    for(String name : BlockSetsHelper.WT_WOOD_NAMES)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
-                }
-
-                if (ModList.get().isLoaded("silverwoodtrees"))
-                {
-                    for(String name : BlockSetsHelper.ST_WOOD_NAMES)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
-                }
-
-                if (ModList.get().isLoaded("missingtrees"))
-                {
-                    for(String name : BlockSetsHelper.MT_WOOD_NAMES)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
-                }
-
-                if (ModList.get().isLoaded("natures_spirit"))
-                {
-                    for(String name : BlockSetsHelper.NSS_WOOD_NAMES)
-                        for (String typeName : BlockSetsHelper.PARQUET_TYPES)
-                            entries.accept(ModBlocks.PARQUET_BLOCKS.get(name+"_"+typeName).get());
-                }
-            }).build());
-
-    public static void register(IEventBus eventBus)
+    @SubscribeEvent
+    public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event)
     {
-        CREATIVE_MODE_TABS.register(eventBus);
+        ASURF_TAB = event.registerCreativeModeTab(ResourceLocation.fromNamespaceAndPath(AestheticSurfaces.MOD_ID, "asurftab"),
+                builder -> builder.icon(() -> new ItemStack(ModBlocks.SIMPLE_CARPETS.get("oak").get().asItem()))
+                        .title(Component.translatable("itemgroup.asurftab")));
     }
 }
